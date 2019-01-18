bf8900f383d375425499517057ae6b63499dfecc
Nodine <xyq608074@163.com>
2019-01-18 16:42:46 +0800

    简单的struct2 helloworld 和 struts.xml简单配置

--- struts/src/struts/helloworld/HelloWorldAction.java
+++ struts/src/struts/helloworld/HelloWorldAction.java
@@ -1,7 +1,7 @@
 package struts.helloworld;
 
-public class HelloWorld {
-    public String HelloAction(){
+public class HelloWorldAction {
+    public String Hello(){
         System.out.println("Hello World !");
         //返回一个字符串 在struts.xml result使用
         return "success";
